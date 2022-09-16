42588e8fdee7300307560547c41a3cadcb21e217
Isioma Ogbuehi <isiomaogbuehi@gmail.com>
2022-09-16 08:53:10 +0100

    Archive and Edit Task (#60)
    Co-authored-by: Isioma Ogbuehi <isiomaogbuehi@Akus-MacBook-Pro.local>

--- app/src/hooks/task/functions.ts
+++ app/src/hooks/task/functions.ts
@@ -2,7 +2,7 @@ import TaskContractInterface from 'contracts/Task.json'
 import TaskStorageContractInterface from 'contracts/TaskStorage.json'
 import { BigNumber } from 'ethers'
 import getContract from 'utils/getContract'
-import { Task } from './types'
+import { Task, ComplexityScore } from './types'
 
 export const openTask = async (
   taskId: number,
@@ -127,4 +127,44 @@ export const createNewTask = async (
 
   const taskId = taskEvent?.args?.[0]?.toNumber() as number
   return taskId
+}
+
+export const archiveTask = async (
+  taskId: number,
+  provider?: any
+): Promise<boolean> => {
+  const contract = getContract(
+    TaskContractInterface.address,
+    TaskContractInterface.abi,
+    provider
+  )
+
+  const response = await contract.archive(taskId)
+  await response.wait()
+
+  return true
+}
+
+export const editTask = async (
+  taskId: number,
+  complexityScore: ComplexityScore,
+  reputationLevel: number,
+  taskDuration: number,
+  provider?: any
+): Promise<boolean> => {
+  const contract = getContract(
+    TaskContractInterface.address,
+    TaskContractInterface.abi,
+    provider
+  )
+
+  const response = await contract.updateTaskRequirement(
+    taskId,
+    complexityScore,
+    reputationLevel,
+    taskDuration
+  )
+  await response.wait()
+
+  return true
 }
