f66bd2c04e3f483c6f2eb2b8c28cb20ecae8c746
Justus Onuoha <valzey87@gmail.com>
2022-10-26 13:12:26 +0100

    Add basic tailwind classes

---
+++ app/src/pages/test.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className='container grid-layout mt-32'>
+      <div className='col-span-3'>
+      <input type='text' className='mt-3 input-base' placeholder='Texty text' />
+      <button className='mt-3 btn-primary'>Hold jfjerugwotergojw</button>
+      <button className='mt-3 btn-outline'>Hold jfjerugwotergojw</button>
+      <button className='mt-3 btn-tag'>Hold jfjerugwotergojw</button>
+      </div>
+    </div>
+  )
+}
+
+export default Home
