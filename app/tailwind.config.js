41c9c7e1592e5bbd14dda136602282890ec42a47
Justus Onuoha <valzey87@gmail.com>
2022-10-26 13:55:28 +0100

    Update font to Manrope

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
@@ -7,17 +9,20 @@ module.exports = {
     extend: {
       borderWidth: {
         DEFAULT: '1px',
-        '0': '0',
-        '2': '2px',
-        '3': '3px',
-        '4': '4px',
-        '6': '6px',
-        '8': '8px',
-        '9': '2.5rem'
+        0: '0',
+        2: '2px',
+        3: '3px',
+        4: '4px',
+        6: '6px',
+        8: '8px',
+        9: '2.5rem'
       },
       container: {
         center: true,
-        padding: '2rem',
+        padding: '2rem'
+      },
+      fontFamily: {
+        sans: ['Manrope', ...defaultTheme.fontFamily.sans]
       }
     }
   },
