8d01d07552ced7ebabf7e785fd79bf1106616989
Isioma Ogbuehi <isiomaogbuehi@Akus-MBP.lan>
2022-11-04 13:42:17 +0100

    Internationalisation Bug Fix

--- app/next-i18next.config.js
+++ app/next-i18next.config.js
@@ -1,7 +1,10 @@
+const path = require('path')
+
 module.exports = {
   i18n: {
     locales: ['en', 'fr'],
     defaultLocale: 'en',
   },
-  reloadOnPrerender: process.env.NODE_ENV === 'development'
+  localePath: path.resolve('./public/locales'),
+  reloadOnPrerender: process.env.NODE_ENV === 'development',
 };
