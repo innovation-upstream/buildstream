25be7992e8112cb7269037123aad70375cf9abbd
Justus Onuoha <valzey87@gmail.com>
2022-11-08 11:01:56 +0100

    Preserve v1 routes (#80)

---
+++ app/src/components/Deposit/LegacyDeposit.tsx
@@ -0,0 +1,158 @@
+import { useWeb3 } from 'hooks'
+import Spinner from 'components/Spinner/Spinner'
+import ERC20 from 'contracts/ERC20.json'
+import Treasury from 'contracts/Treasury.json'
+import { ethers } from 'ethers'
+import { Organization } from 'hooks/organization/types'
+import useTokenInfo from 'hooks/tokenInfo/useTokenInfo'
+import { useState } from 'react'
+import getContract from 'utils/getContract'
+
+interface DepositProps {
+  org: Organization
+}
+
+const LegacyDeposit = ({ org }: DepositProps) => {
+  const orgHasRewardToken = org?.rewardToken !== ethers.constants.AddressZero
+  const [customToken, setCustomToken] = useState(orgHasRewardToken)
+  const [amount, setAmount] = useState(0)
+  const [tokenAddress, setTokenAddress] = useState(
+    orgHasRewardToken ? org?.rewardToken : ''
+  )
+  const { tokenInfo } = useTokenInfo(tokenAddress)
+  const { account, library } = useWeb3()
+  const [isTransacting, setIsTransacting] = useState(false)
+  const [showError, setShowError] = useState(false)
+
+  const handleTokenSelect = (e: any) => {
+    const value = parseInt(e.target.value)
+    setCustomToken(!!value)
+  }
+
+  const processNative = async () => {
+    const signer = library.getSigner()
+    const contract = getContract(Treasury.address, Treasury.abi, signer)
+    const tx = await contract['deposit(uint256)'](org.id, {
+      from: account,
+      value: ethers.utils.parseEther(amount.toString())
+    })
+    await tx.wait()
+  }
+
+  const processCustom = async () => {
+    const signer = library.getSigner()
+    const contract = getContract(Treasury.address, Treasury.abi, signer)
+    const tokenContract = getContract(tokenAddress, ERC20, signer)
+    const approveTx = await tokenContract.approve(
+      Treasury.address,
+      ethers.utils.parseUnits(amount.toString(), tokenInfo?.decimal)
+    )
+    await approveTx.wait()
+    const tx = await contract['deposit(uint256,address,uint256)'](
+      org.id,
+      tokenAddress,
+      ethers.utils.parseUnits(amount.toString(), tokenInfo?.decimal),
+      {
+        from: account
+      }
+    )
+    await tx.wait()
+  }
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault()
+    if ((customToken && !tokenInfo) || amount === 0) {
+      setShowError(true)
+      return
+    }
+    setIsTransacting(true)
+    try {
+      if (customToken) {
+        await processCustom()
+      } else {
+        await processNative()
+      }
+      setAmount(0)
+      setIsTransacting(false)
+      setShowError(false)
+    } catch (e) {
+      console.error(e)
+      setIsTransacting(false)
+    }
+  }
+
+  return (
+    <form className='w-full' onSubmit={handleSubmit}>
+      <h2 className='text-gray-900 text-lg font-medium title-font mb-5'>
+        Deposit to treasury
+      </h2>
+      <div className='mb-3'>
+        {!account && (
+          <p className='text-base text-red-500'>Connect your wallet</p>
+        )}
+      </div>
+      <div className='relative mb-4'>
+        <select
+          className='p-2'
+          value={customToken ? 1 : 0}
+          onChange={handleTokenSelect}
+        >
+          <option value={0}>Native (ETH)</option>
+          <option value={1}>Other</option>
+        </select>
+      </div>
+      {customToken && (
+        <div className='relative mb-4'>
+          <label htmlFor='address' className='leading-7 text-sm text-gray-600'>
+            Token Contract Address
+          </label>
+          <input
+            type='text'
+            id='address'
+            name='address'
+            placeholder={org.rewardToken}
+            required
+            value={tokenAddress}
+            onChange={(e) => setTokenAddress(e.target.value)}
+            className='font-mono w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
+          />
+          {showError && !tokenInfo && (
+            <p className='text-base text-red-500'>Invalid address</p>
+          )}
+        </div>
+      )}
+      <div className='relative mb-4'>
+        <label htmlFor='amount' className='leading-7 text-sm text-gray-600'>
+          Deposit Amount
+          <div className='pointer-events-none absolute flex items-center p-2 text-slate-200'>
+            {customToken ? tokenInfo?.symbol : 'ETH'}
+          </div>
+          <input
+            type='number'
+            id='amount'
+            name='amount'
+            required
+            value={amount}
+            onChange={(e) => setAmount(parseFloat(e.target.value))}
+            placeholder='0.00'
+            className='border-black border-l-9 border-solid font-mono w-full bg-white rounded border focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
+          />
+          {showError && (!amount || amount <= 0) && (
+            <p className='text-base text-red-500'>
+              Amount must be greater than 0
+            </p>
+          )}
+        </label>
+      </div>
+      <button
+        type='submit'
+        disabled={!org.signers.includes(account as string)}
+        className='text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg'
+      >
+        {isTransacting ? <Spinner /> : 'Deposit'}
+      </button>
+    </form>
+  )
+}
+
+export default LegacyDeposit
---
+++ app/src/components/Organization/CreateOrgForm.tsx
@@ -0,0 +1,230 @@
+import Spinner from 'components/Spinner/Spinner'
+import { useWeb3 } from 'hooks'
+import { createOrganization } from 'hooks/organization/functions'
+import { useRouter } from 'next/router'
+import React, { useState } from 'react'
+
+const initialData = {
+  name: '',
+  description: '',
+  approvers: [],
+  signers: [],
+}
+
+type OrgData = typeof initialData & { [keyof: string]: any }
+
+interface Props {
+  onCreate:  (orgId: number) => any
+}
+
+const CreateOrgForm: React.FC<Props> = ({onCreate}) => {
+  const { account, library } = useWeb3()
+  const [status, setStatus] = useState({ text: '', error: false })
+  const [orgData, setOrgData] = useState<OrgData>(initialData)
+  const [processing, setProcessing] = useState(false)
+  const router = useRouter()
+
+  const handleChange = (ev: any) => {
+    const targetName = ev.target.name
+    let targetValue: string | number = ev.target.value
+
+    if (ev.target.type === 'number') {
+      if (targetValue) {
+        targetValue = Number(targetValue)
+      }
+    }
+
+    if (ev.target.dataset.type === 'addressArray') {
+      if (ev.key === 'Enter' && targetValue) {
+        setOrgData((prev: any) => ({
+          ...prev,
+          [targetName]: [...prev[targetName], targetValue],
+        }))
+        ev.target.value = ''
+      }
+      return
+    }
+    setOrgData((prev: any) => ({ ...prev, [targetName]: targetValue }))
+  }
+
+  const deleteTag = (ev: any) => {
+    const index = parseInt(ev.target.id)
+    const targetName = ev.target.dataset.name
+    let addressUpdate = [...orgData[targetName]]
+
+    addressUpdate.splice(index, 1)
+    setOrgData((prev: any) => ({ ...prev, [targetName]: [...addressUpdate] }))
+  }
+
+  const createOrg: any = async (e: any) => {
+    if (!account) {
+      setStatus({ text: 'Wallet Not Connected', error: true })
+      return
+    }
+    if (
+      !orgData.name ||
+      !orgData.description ||
+      orgData.approvers.length === 0 ||
+      orgData.signers.length === 0
+    ) {
+      setStatus({ text: 'Please fill all fields to Create', error: true })
+      return
+    }
+    setProcessing(true)
+    try {
+      const response = await createOrganization(
+        orgData.name,
+        orgData.description,
+        orgData.approvers,
+        orgData.signers,
+        false,
+        library.getSigner()
+      )
+      setProcessing(false)
+      onCreate(response)
+    } catch (e) {
+      console.log(e)
+      setProcessing(false)
+    }
+  }
+
+  return (
+    <div className='lg:w-1/2 md:w-2/3 mx-auto'>
+      <div className='flex flex-wrap -m-2'>
+        <p
+          className={`lg:w-2/3 mx-auto leading-relaxed text-center ${
+            status.error ? 'text-red-500' : 'text-green-500'
+          }`}
+        >
+          {status.text}
+        </p>
+        <div className='p-2 w-full'>
+          <div className='relative'>
+            <label htmlFor='name' className='leading-7 text-sm text-gray-600'>
+              Organization Name
+            </label>
+            <input
+              type='text'
+              id='name'
+              name='name'
+              required
+              value={orgData.name}
+              onChange={handleChange}
+              className='w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
+            />
+          </div>
+        </div>
+        <div className='p-2 w-full'>
+          <div className='relative'>
+            <label
+              htmlFor='reputationLevel'
+              className='leading-7 text-sm text-gray-600'
+            >
+              Approvers
+            </label>
+            <div className='h-auto flex flex-wrap item-center gap-x-1 gap-y-1'>
+              {orgData.approvers.length > 0 &&
+                orgData.approvers.map((approver, index) => {
+                  return (
+                    <div
+                      key={approver}
+                      className='flex items-center border-2 rounded-full px-2 py-1 w-max mb-3'
+                    >
+                      {approver}
+                      <div
+                        onClick={deleteTag}
+                        data-name='approvers'
+                        id={`${index}`}
+                        className='py-1 px-3 ml-3 rounded-full bg-indigo-400 text-red-50 hover:bg-indigo-500 cursor-pointer'
+                      >
+                        x
+                      </div>
+                    </div>
+                  )
+                })}
+            </div>
+            <input
+              type='text'
+              id='approvers'
+              name='approvers'
+              onKeyPress={handleChange}
+              data-type='addressArray'
+              placeholder='Type and Press Enter Key'
+              className='w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
+            />
+          </div>
+        </div>
+        <div className='p-2 w-full'>
+          <div className='relative'>
+            <label
+              htmlFor='reputationLevel'
+              className='leading-7 text-sm text-gray-600'
+            >
+              Signers
+            </label>
+            <div className='h-auto flex flex-wrap item-center gap-x-1 gap-y-1'>
+              {orgData.signers.length > 0 &&
+                orgData.signers.map((signer, index) => {
+                  return (
+                    <div
+                      key={signer}
+                      className='flex items-center border-2 rounded-full px-2 py-1 w-max mb-3'
+                    >
+                      {signer}
+                      <div
+                        onClick={deleteTag}
+                        data-name='signers'
+                        id={`${index}`}
+                        className='py-1 px-3 ml-3 rounded-full bg-indigo-400 text-red-50 hover:bg-indigo-500 cursor-pointer'
+                      >
+                        x
+                      </div>
+                    </div>
+                  )
+                })}
+            </div>
+            <input
+              type='text'
+              id='signers'
+              name='signers'
+              onKeyPress={handleChange}
+              data-type='addressArray'
+              placeholder='Type and Press Enter Key'
+              className='w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
+            />
+          </div>
+        </div>
+        <div className='p-2 w-full'>
+          <div className='relative'>
+            <label
+              htmlFor='message'
+              className='leading-7 text-sm text-gray-600'
+            >
+              Description
+            </label>
+            <textarea
+              id='description'
+              required
+              name='description'
+              value={orgData.description}
+              onChange={handleChange}
+              className='w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out'
+            ></textarea>
+          </div>
+        </div>
+        <div className='p-2 w-full'>
+          <button
+            type='submit'
+            disabled={processing}
+            onClick={createOrg}
+            className='flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg'
+          >
+            {processing ? <Spinner /> : 'Continue'}
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CreateOrgForm
