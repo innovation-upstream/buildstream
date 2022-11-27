25186fc979e2721a85e57395b1e6db87a5d13b5e
Justus Onuoha <valzey87@gmail.com>
2022-11-27 01:03:30 +0100

    Update organization after action execution (#101)

--- graph-api/src/action-contract.ts
+++ graph-api/src/action-contract.ts
@@ -1,18 +1,37 @@
-import { BigInt } from '@graphprotocol/graph-ts'
+import { Address, BigInt } from '@graphprotocol/graph-ts'
 import {
   ActionConfirmation as ActionConfirmationEvent,
   ActionContract as Contract,
   ActionCreation as ActionCreationEvent,
   ActionExecution as ActionExecutionEvent
 } from '../generated/ActionContract/ActionContract'
-import { Action } from '../generated/schema'
+import { Action, Organization } from '../generated/schema'
+import { Organization as OrganizationContract } from '../generated/Organization/Organization'
+
+const organizationAddress = '0xbE4929ae823B0b5E796b27C9567A6098B6173780'
+
+enum ActionType {
+  WITHDRAWAL,
+  ADD_APPROVER,
+  ADD_SIGNER,
+  REMOVE_APPROVER,
+  REMOVE_SIGNER,
+  UPDATE_NAME,
+  UPDATE_DESCRIPTION,
+  UPDATE_REQUIRED_TASK_APPROVALS,
+  UPDATE_REQUIRED_CONFIRMATIONS,
+  UPDATE_REWARD_MULTIPLIER,
+  UPDATE_REWARD_TOKEN,
+  UPDATE_REWARD_SLASH_MULTIPLIER,
+  UPDATE_SLASH_REWARD_EVERY,
+  UPDATE_TAG_REWARD_MULTIPLIER
+}
 
 export function handleActionConfirmation(event: ActionConfirmationEvent): void {
   let entity = Action.load(event.params.actionId.toString())
   if (!entity) return
   let approvers = entity.approvedBy
-  if (approvers == null)
-    approvers = []
+  if (approvers == null) approvers = []
   approvers.push(event.params.sender.toHexString())
   entity.approvedBy = approvers
   entity.save()
@@ -42,4 +61,32 @@ export function handleActionExecution(event: ActionExecutionEvent): void {
   entity.executed = true
   entity.completedAt = event.block.timestamp
   entity.save()
+
+  if (
+    entity.actionType === ActionType.ADD_APPROVER ||
+    entity.actionType === ActionType.ADD_SIGNER ||
+    entity.actionType === ActionType.REMOVE_APPROVER ||
+    entity.actionType === ActionType.REMOVE_SIGNER
+  )
+    return
+
+  const orgId = event.params.orgId
+  const organizationEntity = Organization.load(orgId.toString())
+  if (!organizationEntity) return
+
+  let contract = OrganizationContract.bind(
+    Address.fromString(organizationAddress)
+  )
+  const org = contract.getOrganization(orgId)
+  const orgConfig = contract.getOrganizationConfig(orgId)
+
+  organizationEntity.name = org.name
+  organizationEntity.description = org.description
+  organizationEntity.requiredTaskApprovals = orgConfig.requiredTaskApprovals
+  organizationEntity.requiredConfirmations = orgConfig.requiredConfirmations
+  organizationEntity.rewardMultiplier = orgConfig.rewardMultiplier
+  organizationEntity.rewardToken = orgConfig.rewardToken
+  organizationEntity.rewardSlashMultiplier = orgConfig.rewardSlashMultiplier
+  organizationEntity.slashRewardEvery = orgConfig.slashRewardEvery
+  organizationEntity.save()
 }
